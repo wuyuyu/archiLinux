00038e0a6d02260d24bc7834a057114f9270e0f4
yuyuan <yuyuan.wu@edu.itescia.fr>
2020-04-24 11:11:12 +0200

    STORY 8

--- supervision.sh
+++ supervision.sh
@@ -13,15 +13,21 @@ pgrep generation.sh
 isGeneration=$?
 
 if [ ! -d "$HOME/$4" ]; then
-	echo "le dossier de logs de l'utilisateur n'existe pas"
+	echo "Le dossier de logs de l'utilisateur n'existe pas"
 elif [ $isGeneration -ne 0 ]; then
-	echo "ou si le script generation.sh  n'est pas lancé"
+	echo "Le script generation.sh  n'est pas lancé"
 else
 	echo "Ok ça lance et le dossier existe"
 	sizeOutPut=$(du $HOME/$4/$2 | cut -d '/' -f 1)
 	sizeOutPutError=$(du $HOME/$4/$3 | cut -d '/' -f 1)
 	if [[ $sizeOutPut -ge $5 ]]; then
-		echo "oui ça a dépassé 200"
+		echo "La taille de votre fichier sortie STANDARD: $sizeOutPut K a dépassé $5 K"		
+	elif [[ $sizeOutPutError -ge $5 ]]; then
+		echo "La taille de votre fichier sortie Error: $sizeOutputError K  a dépassé $5 K"
+	else
+		echo "Vos fichier n'a pas dépassé 200K"
+		echo "Taille du fichier sortie standart: $sizeOutPut K"
+		echo "Taille du fichier sortie Error: $sizeOutPutError K"
 	fi
-
+	
 fi
